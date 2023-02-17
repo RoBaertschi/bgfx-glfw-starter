a4c2d9ff6d8bad297a0da1b37ab21b40281c2542
RoBaertschi <rtmbaertschi007@gmail.com>
2023-02-17 16:10:29 +0100

    Added Dependencies

---
+++ bgfx-glfw-starter.cpp
@@ -0,0 +1,12 @@
+﻿// bgfx-glfw-starter.cpp : Defines the entry point for the application.
+//
+
+#include "bgfx-glfw-starter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
